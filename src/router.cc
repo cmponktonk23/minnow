40f9ec2a176597eecf43ea3e800f5b79b0409a51
ponktonk <cmponktonk23@gmail.com>
2025-11-04 06:49:55 +0000

    add comments

--- src/router.cc
+++ src/router.cc
@@ -26,21 +26,30 @@ void Router::add_route( const uint32_t route_prefix,
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
+  // Iterate through all interfaces and try to send all their incoming datagrams
   for ( auto& _interface : interfaces_ ) {
     auto& queue = _interface->datagrams_received();
     while ( !queue.empty() ) {
       auto dgram = queue.front();
       queue.pop();
       uint32_t dst_ip = dgram.header.dst;
+      
       Rule matched_rule;
       bool first = true;
       matched_rule.prefix_length = 0;
       for ( auto& rule : route_table_ ) {
+        /**
+         * Bug: 0.0.0.0/0 should match with any ip, but its prefix_length = 0 (use first to handle this case)
+         */
         if ( match( rule, dst_ip ) && ( rule.prefix_length > matched_rule.prefix_length || first ) ) {
           matched_rule = rule;
           first = false;
         }
       }
+      /**
+       * Bug1: 0.0.0.0/0 should match with any ip, but its prefix_length = 0 (use first to handle this case)
+       * Bug2: datagram can be sent only if ttl >= 2
+       */
       if ( ( matched_rule.prefix_length > 0 || !first ) && dgram.header.ttl > 1 ) {
         auto next_hop = matched_rule.next_hop.has_value() ? matched_rule.next_hop.value() : Address::from_ipv4_numeric( dst_ip );        
         auto matched_interface = interface( matched_rule.interface_num );
@@ -53,6 +62,9 @@ void Router::route()
 
 auto Router::match( const Rule& rule, const uint32_t ip ) -> bool 
 {
+  /**
+   * Bug: 0.0.0.0/0 should match with any ip, but its prefix_length = 0
+   */
   if ( rule.prefix_length == 0 ) return true;
   uint8_t shift = 32 - rule.prefix_length;
   return (rule.route_prefix >> shift) == (ip >> shift);
